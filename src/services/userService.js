5717ebac19ca0f71f3190bb64a0c543a2abf22f7
dev-yoga <spin_shoppe@icloud.com>
2021-10-15 12:58:43 -0700

    I fixed it on the backend but I made some changes to userService for no particular reason I think

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,15 +1,16 @@
 import request from 'superagent';
 
-const baseUrl = process.env.FLOW_API
+// const baseUrl = process.env.FLOW_API
+// const baseUrl = 'http://localhost:1377/api'
 
-export const signUp = (username, password) => request.post(`${baseUrl}/signup`, {username, password,}).then(res => res.body);
+export const signUp = (username, password) => request.post('http://localhost:1377/api/signup', {username, password,}).then(res => res.body);
 
-export const signIn = (username, password) => request.post(`${baseUrl}/signin`, {
+export const signIn = (username, password) => request.post('http://localhost:1377/api/signin', {
     username,
     password,
 }).then(res => {
     return {
-                user_id: res.body.user_id,
+        user_id: res.body.user_id,
         token: res.body.token
     }
 })
